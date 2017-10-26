c44230b5228fb11cbbd7d562881e2f9c1de14323
Gaurav Ahuja <gauravahuja.me@gmail.com>
2017-10-26 05:41:18 +0530

    Fix api

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/post',function(req, res){
-    var id = req.body.id;
+    var id = req.query.id;
     var data = {
         "error": 1,
         "posts": ""
